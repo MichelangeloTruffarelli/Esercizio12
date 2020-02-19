95943477f4cff7320b96f7111b8885f67662c6d2
studenti <studenti@rosselli.loc>
2020-02-19 13:24:42 +0100

    Add project files.

---
+++ BubbleSort/BubbleSort1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    public class BubbleSort1
+    {
+        public static void Sort(int[]array)
+        {
+            int x, y;
+            int temp;
+            for (x = 0; x < array.Length; x++)
+            {
+                for(y=0; y< array.Length - 1; y++)
+                {
+                    if(array[y]> array[x + 1])
+                    {
+                        temp = array[y];
+                        array[y] = array[y + 1];
+                        array[y + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppBS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensione = { 1, 20, 50, 100, 500, 1000 };
+            using (StreamWriter sw = new StreamWriter("stat.csv", false, Encoding.UTF8))
+            {
+                sw.WriteLine("Algoritmo, Dimensione, Tipo");
+                foreach (int dim in dimensione)
+                {
+                    int[] numeri = new int[dim];
+                    Random random = new Random();
+                    for (int i = 0; i < dim; i++)
+                        numeri[i] = random.Next();
+                    Stopwatch s = new Stopwatch();
+                    s.Start();
+                    BubbleSort.Sort(numeri);
+                    s.Stop();
+                    long elapsed = s.ElapsedMilliseconds;
+                    sw.WriteLine($"BubbleSort;{dim};{elapsed}");
+                }
+                sw.Flush();
+                Console.ReadLine();
+            }
+        }
+    }
+}
